378ac40903daa05d7baa65eb5c2ba1a54d06f35f
TWalk007 <walkertb03@gmail.com>
2018-05-16 12:38:26 -0400

    Created the OpenDoor() function in OpenDoor.cpp.  Then created the ActorThatMoves to hold the default pawn at runtime.

--- BuildingEscape/Source/BuildingEscape/Private/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/Private/OpenDoor.cpp
@@ -2,6 +2,7 @@
 
 #include "OpenDoor.h"
 #include "GameFramework/Actor.h"
+#include "Engine/World.h"
 
 
 // Sets default values for this component's properties
@@ -19,19 +20,27 @@ UOpenDoor::UOpenDoor()
 void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
-
-	AActor* Owner = GetOwner();
-	FRotator NewRotation = FRotator(0.f,60.f,0.f);
-	Owner->SetActorRotation(NewRotation);
 	
+	ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
 }
-
 
 // Called every frame
 void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	// Poll the Trigger Volume every frame
+	// If the ActorThatOpens is in the volume,
+	if (PressurePlate->IsOverlappingActor(ActorThatOpens))
+	{
+		OpenDoor();
+	}
+
 }
 
+void UOpenDoor::OpenDoor()
+{
+	AActor* Owner = GetOwner();
+	FRotator NewRotation = FRotator(0.f, 60.f, 0.f);
+	Owner->SetActorRotation(NewRotation);
+}
